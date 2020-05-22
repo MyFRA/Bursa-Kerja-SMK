b801ab14987d268fbe8819c77b261c64210191c9
MyFRA <myfragithub@gmail.com>
2020-05-22 22:38:39 +0700

    update data seeds

--- database/seeds/BidangKeahlianTableSeeder.php
+++ database/seeds/BidangKeahlianTableSeeder.php
@@ -28,36 +28,36 @@ class BidangKeahlianTableSeeder extends Seeder
         		'kode' => 'TIdK',
         		'nama' => 'Teknologi Informasi dan Komunikasi',
 			],
-			[
-        		'id'   => 4,
-        		'kode' => 'KdPS',
-        		'nama' => 'Kesehatan dan Pekerjaan Sosial',
-			],
-			[
-        		'id'   => 5,
-        		'kode' => 'AdA',
-        		'nama' => 'Agribisnis dan Agroteknologi',
-			],
-			[
-        		'id'   => 6,
-        		'kode' => 'Kmrtmn',
-        		'nama' => 'Kemaritiman',
-			],
-			[
-        		'id'   => 7,
-        		'kode' => 'BdM',
-        		'nama' => 'Bisnis dan Manajemen',
-			],
-			[
-        		'id'   => 8,
-        		'kode' => 'Prwst',
-        		'nama' => 'Pariwisata',
-			],
-			[
-        		'id'   => 9,
-        		'kode' => 'SdIK',
-        		'nama' => 'Seni dan Industri Kreatif',
-			]
+			// [
+        	// 	'id'   => 4,
+        	// 	'kode' => 'KdPS',
+        	// 	'nama' => 'Kesehatan dan Pekerjaan Sosial',
+			// ],
+			// [
+        	// 	'id'   => 5,
+        	// 	'kode' => 'AdA',
+        	// 	'nama' => 'Agribisnis dan Agroteknologi',
+			// ],
+			// [
+        	// 	'id'   => 6,
+        	// 	'kode' => 'Kmrtmn',
+        	// 	'nama' => 'Kemaritiman',
+			// ],
+			// [
+        	// 	'id'   => 7,
+        	// 	'kode' => 'BdM',
+        	// 	'nama' => 'Bisnis dan Manajemen',
+			// ],
+			// [
+        	// 	'id'   => 8,
+        	// 	'kode' => 'Prwst',
+        	// 	'nama' => 'Pariwisata',
+			// ],
+			// [
+        	// 	'id'   => 9,
+        	// 	'kode' => 'SdIK',
+        	// 	'nama' => 'Seni dan Industri Kreatif',
+			// ]
         ];
 
         foreach( $bidangKeahlian as $bk ) {
